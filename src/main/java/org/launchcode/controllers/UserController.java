90b9e44b1e3e631d25953b8200bfd0b146d270e9
jmdaku <jmdaku@gmail.com>
2023-10-05 21:31:42 -0400

    added user pages

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,40 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm(){
+        return "user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("use",user);
+        model.addAttribute("username",user.getUsername());
+        model.addAttribute("email",user.getEmail());
+        model.addAttribute("username",user.getPassword());
+        model.addAttribute("verify",verify);
+
+        if(user.getPassword().equals(verify)) {
+            return "user/index";
+        }else {
+            model.addAttribute("error","Invalid: Passwords do not match");
+            model.addAttribute("username",user.getUsername());
+            model.addAttribute("email",user.getEmail());
+            return "user/add";
+        }
+
+    }
+
+
+}

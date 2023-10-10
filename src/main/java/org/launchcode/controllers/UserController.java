84472b8087452dc8cf65ae7acebe6c7b9191f857
jmdaku <jmdaku@gmail.com>
2023-10-09 21:31:29 -0400

    updated to add verification

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,22 +20,31 @@ public class UserController {
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("use",user);
-        model.addAttribute("username",user.getUsername());
-        model.addAttribute("email",user.getEmail());
-        model.addAttribute("username",user.getPassword());
-        model.addAttribute("verify",verify);
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, String verify, Errors errors) {
+        if(errors.hasErrors()){
 
-        if(user.getPassword().equals(verify)) {
-            return "user/index";
-        }else {
-            model.addAttribute("error","Invalid: Passwords do not match");
-            model.addAttribute("username",user.getUsername());
-            model.addAttribute("email",user.getEmail());
             return "user/add";
-        }
+        }else {
 
+            model.addAttribute("user", user);
+//            model.addAttribute("username", user.getUsername());
+//            model.addAttribute("email", user.getEmail());
+//            model.addAttribute("username", user.getPassword());
+            model.addAttribute("verify", verify);
+            //model.addAttribute(new User());
+
+            if (user.getPassword().equals(verify)) {
+                return "user/index";
+            } else {
+                model.addAttribute("error", "Invalid: Passwords do not match");
+                model.addAttribute("username", user.getUsername());
+                model.addAttribute("email", user.getEmail());
+                model.addAttribute(new User());
+                return "user/add";
+
+            }
+
+        }
     }
 
 
